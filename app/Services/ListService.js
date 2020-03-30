efe76e07e829efecb4a2b11caa3eb69ddb2c15de
AshleyQSmith <smittyq@gmail.com>
2020-03-29 20:59:59 -0600

    added delete confirmation

--- app/Services/ListService.js
+++ app/Services/ListService.js
@@ -15,8 +15,11 @@ class ListService {
   }
 
   delete(listId) {
+    let del = confirm("Confirm Delete");
+    if (del == true) {
     _store.State.lists = _store.State.lists.filter(list => list.id != listId)
     _store.saveState()
+    }
   }
 
 // remove by index not id
@@ -28,10 +31,13 @@ class ListService {
   }
 
   deleteTask(task, listID) {
+    let del = confirm("Confirm Delete");
+    if (del == true) {
     let list = _store.State.lists.find(list => list.id == listID)
     let deletedTask = list.tasks.find(list => list.tasks == task)
-    list.tasks.splice(task)
+    // list.tasks.splice(task)
     _store.saveState()
+    }
   }
 
 }
